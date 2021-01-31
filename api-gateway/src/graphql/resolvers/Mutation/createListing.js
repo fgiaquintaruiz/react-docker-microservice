4bbeaa652b29c7c2a76f1a32eb66c71db4c7f4d2
fgiaquintaruiz <fgiaquintaruiz@gmail.com>
2021-01-31 16:12:32 -0300

    git video part 8

--- api-gateway/src/graphql/resolvers/Mutation/createListing.js
+++ api-gateway/src/graphql/resolvers/Mutation/createListing.js
@@ -2,7 +2,8 @@ import ListingService from "#root/adapters/ListingsService";
 
 const createListingResolver = async (obj, { description, title }, context) => {
 
-    if (!context.res.locals.userSession) throw new Error("Not logged in!");
+    //TODO the cookie is not being added to chrome in production env, workaround pending
+    // if (!context.res.locals.userSession) throw new Error("Not logged in!");
 
     console.log("createListingResolver description: " + description + " title: " + title);
     return await ListingService.createListing({ description, title });
